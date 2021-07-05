1c528f92729bdf09d36e37559d4b230c0e1c5363
Nate Denys <nate_denys@yahoo.com>
2021-07-04 18:59:17 -0700

    reupload of code with Configuration
    Added configuration for public path and added PWA plugin

---
+++ precache-manifest.5dd564454e729545b649029890fb2532.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd009de84c07c140aaa1cbcd0587cf55",
+    "url": "/gameready/attpark.jpg"
+  },
+  {
+    "revision": "4d35344f328efadaa9ce",
+    "url": "/gameready/css/app.4ec8941d.css"
+  },
+  {
+    "revision": "46accfcf7a75121c6caf",
+    "url": "/gameready/css/chunk-vendors.d48dbef4.css"
+  },
+  {
+    "revision": "b49b2c0ed9f4ed9e236017d2df73b6d8",
+    "url": "/gameready/index.html"
+  },
+  {
+    "revision": "d6b90ef61acfd0036f5b",
+    "url": "/gameready/js/about.5f7e8a1d.js"
+  },
+  {
+    "revision": "4d35344f328efadaa9ce",
+    "url": "/gameready/js/app.b9c8c033.js"
+  },
+  {
+    "revision": "46accfcf7a75121c6caf",
+    "url": "/gameready/js/chunk-vendors.6996fa11.js"
+  },
+  {
+    "revision": "b82348c106e63c8ef50c4efd338e775c",
+    "url": "/gameready/manifest.json"
+  },
+  {
+    "revision": "c8f594f7057ee6de512ded70760d5a85",
+    "url": "/gameready/presid.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/gameready/robots.txt"
+  },
+  {
+    "revision": "5fbcc9df9696b499f0c518c5c0baee54",
+    "url": "/gameready/sflogo.jpg"
+  }
+]);
